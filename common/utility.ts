921aad7a44ee2af594c3e0af3615e916913e7147
Daniel Lawson <lawblaze4@gmail.com>
2024-10-10 02:00:24 +0100

    made some changes

--- common/utility.ts
+++ common/utility.ts
@@ -2,9 +2,9 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 export const dbConfig: any = {
-  dbname: process.env.DBNAME,
+  dbname: process.env.DB_NAME,
   dbport: process.env.DBPORT,
   dbhost: process.env.DB_HOST,
-  dbuser: process.env.DBUSER,
-  dbpassword: process.env.DBPASSWORD,
+  dbuser: process.env.DB_USER,
+  dbpassword: process.env.DB_PASSWORD,
 };
