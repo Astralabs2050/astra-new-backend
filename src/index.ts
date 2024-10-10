8658e589bdf44d0ec18bbab9b81c9b64f007a886
Daniel Lawson <lawblaze4@gmail.com>
2024-10-10 11:15:58 +0100

    mades some changes

--- src/index.ts
+++ src/index.ts
@@ -46,7 +46,7 @@ async function startServer() {
   // Routes
   app.use(routes);
   const PORT = process.env.PORT || 3000;
-  httpServer.listen(3001, () => {
+  httpServer.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     console.log(`connect http://localhost:${PORT}`);
   });
